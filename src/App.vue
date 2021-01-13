fe9a44a5832db119da6420e44c0e16cf1cd93b91
linchengyang <18960807585@163.com>
2021-01-13 19:52:33 +0800

    调节背景颜色(Adjust the background color)

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@ export default {
   position: absolute;
   top: 0;
   left: 0;
-  background: linear-gradient(180deg, #2D1E2B 0%, #171B1D 100%) ;
+  background: radial-gradient(circle 700px at 50% 0%,#2D1E2B 50%, #171B1D 100%) ;
   margin: 0;
 }
 
